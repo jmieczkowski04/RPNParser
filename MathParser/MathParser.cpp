8c367ec05068a64716fd558390d9376dcc374093
Jakub Mieczkowski <jmieczkowski04@gmail.com>
2023-03-18 11:57:44 +0100

    Full support for normal polish notation

--- MathParser/MathParser.cpp
+++ MathParser/MathParser.cpp
@@ -10,9 +10,12 @@
 bool GReversedOrder = true;
 
 
-int main()
+int main(int argc, char** argv)
 {
-    
+    for (int i = 1; i < argc; i++)
+        if (!strcmp(argv[i], "--pn"))
+            GReversedOrder = false;
+
     std::vector<std::string> elements;
 
     std::string input;
