2bf013c59f5521dea28cf51348450aafe817847f
Jakub Mieczkowski <jmieczkowski04@gmail.com>
2023-03-23 23:46:15 +0100

    new commandline user interface

--- RPNParser/RPNParser.cpp
+++ RPNParser/RPNParser.cpp
@@ -8,7 +8,7 @@
 #include "ParseHelper.h"
 
 bool GReversedOrder = true;
-
+std::string SwitchNotation(std::string& notation);
 
 int main(int argc, char** argv)
 {
@@ -16,31 +16,63 @@ int main(int argc, char** argv)
         if (!strcmp(argv[i], "--pn"))
             GReversedOrder = false;
 
-    std::vector<std::string> elements;
+    std::string notation;
+    SwitchNotation(notation);
 
+    std::string promptChar = "> ";
+
+    std::cout << "RPNParser. Parsing using " << notation << " Polish notation. To end type \'exit\'. To switch notation type \'switch notation\'.\n" << promptChar;
+
+    std::vector<std::string> elements;
     std::string input;
     std::getline(std::cin, input);
 
-    std::string element;
-    std::istringstream inputStream(input);
-    while (std::getline(inputStream, element, ' '))
+    while (input != "exit")
     {
-        elements.push_back(element);
-    }
+        if (input == "switch notation")
+        {
+            GReversedOrder = !GReversedOrder;
+            std::cout << "Switched notation from " << notation << " to " << SwitchNotation(notation) << "\n" << promptChar;
+            std::getline(std::cin, input);
+            continue;
+        }
 
-    if (!GReversedOrder)
-    {
-        std::reverse(elements.begin(), elements.end());
+        elements.clear();
+
+        std::string element;
+        std::istringstream inputStream(input);
+        while (std::getline(inputStream, element, ' '))
+        {
+            elements.push_back(element);
+        }
+
+        if (!GReversedOrder)
+        {
+            std::reverse(elements.begin(), elements.end());
+        }
+
+
+        Expression* exp = CreateExpression(elements);
+        if (exp)
+        {
+            exp->Parse(elements);
+            std::cout << exp->GetValue() << "\n" << promptChar;
+            delete exp;
+        }
+        else
+        {
+            std::cout << "Failed to parse\n" << promptChar;
+        }
+
+        std::getline(std::cin, input);
     }
 
     
-    Expression* exp = CreateExpression(elements);
-    if (exp)
-    {
-        exp->Parse(elements);
-        std::cout << exp->GetValue();
-    }
-    else 
-        std::cout << "Failed to parse";
 
+}
+
+std::string SwitchNotation(std::string& notation)
+{
+    notation = GReversedOrder ? "Reverse" : "Normal";
+    return notation;
 }
